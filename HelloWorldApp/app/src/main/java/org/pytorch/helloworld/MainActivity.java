15d39303a2b4a4225500ce82f56920bcc5f57624
Robin Dong <rdong@woolworths.com.au>
2020-05-13 15:42:53 +1000

    Use RegNet(without Squeeze-Excitation, since the nn.Sigmoid() works strangely on Android emulator) to classify world-wide-birds

--- HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -17,11 +17,70 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.nio.FloatBuffer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static void checkOutBufferCapacity(
+      FloatBuffer outBuffer, int tensorWidth, int tensorHeight) {
+    if (3 * tensorWidth * tensorHeight > outBuffer.capacity()) {
+      throw new IllegalStateException("Buffer underflow");
+    }
+  }
+
+  public static void bitmapToFloatBuffer(
+      final Bitmap bitmap,
+      final int width,
+      final int height,
+      final FloatBuffer outBuffer) {
+    checkOutBufferCapacity(outBuffer, width, height);
+
+    final int pixelsCount = height * width;
+    final int[] pixels = new int[pixelsCount];
+    bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+    final int offset_g = pixelsCount;
+    final int offset_b = 2 * pixelsCount;
+    for (int i = 0; i < pixelsCount; i++) {
+      final int c = pixels[i];
+      float r = ((c >> 16) & 0xff);
+      float g = ((c >> 8) & 0xff);
+      float b = ((c) & 0xff);
+      outBuffer.put(i, b);
+      outBuffer.put(offset_g + i, g);
+      outBuffer.put(offset_b + i, r);
+    }
+  }
+
+  public static Tensor bitmapToFloat32Tensor(final Bitmap bitmap) {
+    int width = bitmap.getWidth();
+    int height = bitmap.getHeight();
+    final FloatBuffer floatBuffer = Tensor.allocateFloatBuffer(3 * width * height);
+    bitmapToFloatBuffer(bitmap, width, height, floatBuffer);
+    Log.e("floatBuffer[0]", Float.toString(floatBuffer.get(0)));
+    Log.e("floatBuffer[1]", Float.toString(floatBuffer.get(1)));
+    Log.e("floatBuffer[2]", Float.toString(floatBuffer.get(2)));
+    return Tensor.fromBlob(floatBuffer, new long[] {1, 3, height, width});
+  }
+
+  private static String[] readLabels(InputStream is) throws IOException {
+    List<String> lines = new ArrayList<String>();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    while (reader.ready()) {
+      String line = reader.readLine();
+      lines.add(line);
+    }
+    Log.e("Pytorch", Long.toString(lines.size()));
+    return lines.toArray(new String[lines.size()]);
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +94,13 @@ public class MainActivity extends AppCompatActivity {
       // app/src/main/assets/image.jpg
       bitmap = BitmapFactory.decodeStream(getAssets().open("image.jpg"));
       // loading serialized torchscript module from packaged into app android asset model.pt,
-      // app/src/model/assets/model.pt
+      // app/src/main/assets/model.pt
       module = Module.load(assetFilePath(this, "model.pt"));
+      if (module != null) {
+          Log.e("module", "correct");
+      }
+      InputStream is = getAssets().open("words.txt");
+      ImageNetClasses.IMAGENET_CLASSES = readLabels(is);
     } catch (IOException e) {
       Log.e("PytorchHelloWorld", "Error reading assets", e);
       finish();
@@ -47,9 +111,7 @@ public class MainActivity extends AppCompatActivity {
     imageView.setImageBitmap(bitmap);
 
     // preparing input tensor
-    final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
-        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-
+    final Tensor inputTensor = bitmapToFloat32Tensor(bitmap);
     // running the model
     final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
 
@@ -59,12 +121,17 @@ public class MainActivity extends AppCompatActivity {
     // searching for the index with maximum score
     float maxScore = -Float.MAX_VALUE;
     int maxScoreIdx = -1;
+    Log.e("scores[0]", Float.toString(scores[0]));
+    Log.e("scores[1]", Float.toString(scores[1]));
+    Log.e("scores[2]", Float.toString(scores[2]));
+    Log.e("scores[1000-1]", Float.toString(scores[1000-1]));
     for (int i = 0; i < scores.length; i++) {
       if (scores[i] > maxScore) {
         maxScore = scores[i];
         maxScoreIdx = i;
       }
     }
+    Log.e("maxScoreIdx", Integer.toString(maxScoreIdx));
 
     String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
 
